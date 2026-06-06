840c2b2a2551e51ad8101334c42fdcd3ddb5248d
nicholaschangIT <nicholasc001@hotmail.com>
2026-06-06 12:38:31 +0100

    add alert to tf

--- infra/import.tf
+++ infra/import.tf
@@ -1,10 +1,9 @@
-# Import existing apps when they already exist in Azure but are missing from state.
 import {
-  to = azurerm_container_app.orders_api[0]
-  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.App/containerApps/orders-api"
+  id = "/subscriptions/64681aa1-73cc-4155-aa97-caa0e4837412/resourceGroups/rg-sre-lab/providers/microsoft.alertsmanagement/smartDetectorAlertRules/Failure Anomalies - ai-51a0c59340d39"
+  to = azurerm_monitor_smart_detector_alert_rule.failure_anomalies
 }
 
 import {
-  to = azurerm_container_app.change_lookup[0]
-  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.App/containerApps/change-lookup"
+  id = "/subscriptions/64681aa1-73cc-4155-aa97-caa0e4837412/resourceGroups/rg-sre-lab/providers/microsoft.insights/actiongroups/Application Insights Smart Detection"
+  to = azurerm_monitor_action_group.ai_smart_detection
 }
