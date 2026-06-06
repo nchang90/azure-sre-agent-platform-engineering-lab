38bb20da9043ebb7e9dcc3f4f782596241f5c7ff
nicholaschangIT <nicholasc001@hotmail.com>
2026-06-06 13:11:31 +0100

    delete .terraform folder

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,7 +13,6 @@ terraform {
   }
 
   backend "azurerm" {
-    subscription_id       = "64681aa1-73cc-4155-aa97-caa0e4837412"
     resource_group_name  = "terraform-tfstate-rg"
     storage_account_name = "terraformstatesbox"
     container_name       = "tfstate"
