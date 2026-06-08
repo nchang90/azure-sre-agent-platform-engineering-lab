291bd5b161b7db19a6424996e16d8f381ae49052
nicholaschangIT <nicholasc001@hotmail.com>
2026-06-08 21:26:29 +0100

    add import block

--- infra/import.tf
+++ infra/import.tf
@@ -1,4 +1,4 @@
 import {
   to = azapi_resource.sre_agent
-  id = "/subscriptions/64681aa1-73cc-4155-aa97-caa0e4837412/resourceGroups/rg-sre-lab/providers/Microsoft.App/agents/sre-agent"
+  id = "/subscriptions/64681aa1-73cc-4155-aa97-caa0e4837412/resourceGroups/rg-sre-lab/providers/Microsoft.App/agents/sre-agent?api-version=2026-01-01"
 }
